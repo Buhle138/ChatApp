ff070389653fb76aca0cfefdf22afdc8f26193b5
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-24 22:00:59 +0200

    creating both the create account and the login account.

--- ChatApp/ContentView.swift
+++ ChatApp/ContentView.swift
@@ -10,22 +10,72 @@ import SwiftUI
 struct ContentView: View {
     
     @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
     
     var body: some View {
         NavigationView {
             ScrollView{
-                Picker(selection: $isLoginMode) {
-                    Text("Login")
-                    Text("Create Account")
-                } label: {
-                   Text("Picker Here")
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding()
+                
+                VStack(spacing: 14){
+                    
+                    Picker(selection: $isLoginMode) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    } label: {
+                       Text("Picker Here")
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding()
+                    
+                    if isLoginMode{
+                        Button{
+                            
+                        }label: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 64))
+                        }
+                    }
+                    
+                   
+                    
+                    
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .padding(12)
+                        .background(.white)
+                       
+                    SecureField("Password", text: $password)
+                        .padding(12)
+                        .background(.white)
+                    
+                    Button{
+                        
+                    }label: {
+                        HStack{
+                            
+                            Spacer()
+                            Text(isLoginMode ? "Login" : "Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                            Spacer()
+                        }.background(Color.blue)
+                    }
+                    
 
-                Text("Here is my creation account page")
-            }
-            .navigationTitle("Create Account")
+                   
+                }
+                    
+                }
+                .padding()
+                .background(Color(.init(white: 0, alpha: 0.05)))
+            
+                
+             
+                .navigationTitle(isLoginMode ? "Login" : "Create Account")
         }
     }
 }
