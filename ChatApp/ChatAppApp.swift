b1b73cd99c3e21074de1004e9e5f2f7b48dba9aa
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-24 19:53:45 +0200

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Buhle Radzilani on 2024/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
