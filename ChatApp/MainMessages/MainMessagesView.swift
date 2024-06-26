69fff78354f7350f45024abf5664ed2ed891486e
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-27 01:50:55 +0200

    Adding the messages view so that we can create the template for the messages view.

---
+++ ChatApp/MainMessages/MainMessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MainMessagesView.swift
+//  ChatApp
+//
+//  Created by Buhle Radzilani on 2024/06/27.
+//
+
+import SwiftUI
+
+struct MainMessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainMessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainMessagesView()
+    }
+}
