1061dd7d5f60d161f7479fbb74fc3de23376f113
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-27 02:08:18 +0200

    current template with user profile icon and dummy text to represent the messages.

--- ChatApp/MainMessages/MainMessagesView.swift
+++ ChatApp/MainMessages/MainMessagesView.swift
@@ -9,7 +9,39 @@ import SwiftUI
 
 struct MainMessagesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            
+            //Nav bar
+            VStack{
+                ScrollView{
+                    ForEach(0..<10, id: \.self){num in
+                        
+                        VStack{
+                            
+                            HStack(spacing: 16){
+                               Image(systemName: "person.fill")
+                                    .font(.system(size: 32))
+                                VStack(alignment: .leading){
+                                    Text("Username")
+                                    Text("Message sent to user")
+                                }
+                                Spacer()
+                                Text("22d")
+                                    .font(.system(size: 14, weight: .semibold))
+                            }
+                            Divider()
+                        }.padding(.horizontal)
+                        
+                        
+                    }
+                    
+                }
+                
+            }
+            .navigationTitle("Main Messages View")
+    
+        }
+       
     }
 }
 
