688425bd3457377701afecb737fc6f9c213b0f6e
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-27 13:54:41 +0200

    styling the message view

--- ChatApp/MainMessages/MainMessagesView.swift
+++ ChatApp/MainMessages/MainMessagesView.swift
@@ -10,9 +10,38 @@ import SwiftUI
 struct MainMessagesView: View {
     var body: some View {
         NavigationView {
-            
             //Nav bar
             VStack{
+                
+                HStack(spacing: 16){
+                    
+                    Image(systemName: "person.fill")
+                        .font(.system(size: 24, weight: .heavy))
+                    VStack(alignment: .leading, spacing: 4){
+                        Text("USERNAME")
+                            .font(.system(size: 24, weight: .bold))
+                        
+                        HStack{
+                            Circle()
+                                .foregroundColor(.green)
+                                .frame(width: 14, height: 14)
+                            Text("online")
+                                .font(.system(size: 12))
+                                .foregroundColor(Color(.lightGray))
+                           
+                        }
+                        
+                      
+                    }
+                    
+                    Spacer()
+                    Image(systemName: "gear")
+                   
+                }
+                .padding()
+                
+                
+                
                 ScrollView{
                     ForEach(0..<10, id: \.self){num in
                         
@@ -38,7 +67,7 @@ struct MainMessagesView: View {
                 }
                 
             }
-            .navigationTitle("Main Messages View")
+            .navigationBarHidden(true)
     
         }
        
